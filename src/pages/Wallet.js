6b09846116985886381e3ffc02eee3ae989c29f9
MauroMarchesan88 <mauromarchesan88@gmail.com>
2022-04-01 15:28:20 -0300

    Requisito 5 completo

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -24,14 +24,43 @@ class Wallet extends React.Component {
     return (
       <div>
         TrybeWallet
-        <p data-testid="email-field">{user.email}</p>
-        <div data-testid="total-field">0</div>
-        <div data-testid="header-currency-field">BRL</div>
-        <div>
-          {
-            currencies && currencies.map((currency) => <p key={ currency }>{currency}</p>)
-          }
-        </div>
+        <section>
+          <p data-testid="email-field">{user.email}</p>
+          <p data-testid="total-field">0</p>
+          <div data-testid="header-currency-field">BRL</div>
+        </section>
+        <form>
+          <input type="number" data-testid="value-input" placeholder="Valor" />
+          <input type="text" data-testid="description-input" placeholder="Descrição" />
+          <label htmlFor="currency-input">
+            Moeda
+            <select data-testid="currency-input" id="currency-input">
+              {
+                currencies
+                && currencies.map(
+                  (currency) => <option key={ currency }>{currency}</option>,
+                )
+              }
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            <select data-testid="method-input" id="method-input">
+              <option value="dinheiro">Dinheiro</option>
+              <option value="cartaoCredito">Cartão de crédito</option>
+              <option value="cartaoDebito">Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="expenseTag">
+            <select data-testid="tag-input">
+              <option value="alimentacao">Alimentação</option>
+              <option value="lazer">Lazer</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="transporte">Transporte</option>
+              <option value="saude">Saúde</option>
+            </select>
+          </label>
+        </form>
+        <section />
       </div>
     );
   }
